2c261aca1d83ac8faebe19ff5a02202847c39a4c
Nole88rahmou <118311738+Nole88rahmou@users.noreply.github.com>
2023-08-02 08:50:26 -0400

    premiere commit

---
+++ components/footer/footer.js
@@ -0,0 +1,15 @@
+import "./footer.css";
+import Link from "next/link";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <Link href="https://github.com/Nole88rahmou">GitHub</Link>
+      <Link href="https://www.linkedin.com/in/meradji-abderrahim-3bb444251">
+        LinkedIn
+      </Link>
+    </footer>
+  );
+}
+
+export default Footer;
