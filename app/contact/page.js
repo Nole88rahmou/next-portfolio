557d42d15aad5350c6415dd9fe73a7646023c36f
Nole88rahmou <118311738+Nole88rahmou@users.noreply.github.com>
2023-08-02 14:45:20 -0400

    Mise a jour finale

--- app/contact/page.js
+++ app/contact/page.js
@@ -10,7 +10,10 @@ function page() {
     message: "",
   });
 
-  const [message, setMessage] = useState("");
+  const [errorNom, setErrorNom] = useState("");
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -22,58 +25,74 @@ function page() {
   };
 
   const validateEmail = (email) => {
-    var re =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(email);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
+    
+    if (state.nom === "") {
+      setErrorNom("Veuillez entrer un nom.");
+    } else {
+      setErrorNom("");
+    }
+    if (state.message === "") {
+      setErrorMessage("Veuillez entrer un message.");
+    } else {
+      setErrorMessage("");
+    }
     if (!validateEmail(state.email)) {
-      setMessage("Veuillez entrer une adresse email valide.");
-      return;
+      setErrorEmail("Veuillez entrer une adresse email valide.");
+    } else {
+      setErrorEmail("");
     }
 
-    console.log(state);
+    if (state.nom !== "" && validateEmail(state.email) && state.message !== "") {
+      // Définir le message de confirmation
+      setSuccessMessage("Votre demande a été envoyée.");
 
-    // Définir le message de confirmation
-    setMessage("Votre demande a été envoyée.");
-    setState({ nom: "", email: "", message: "" }); // Reset des champs du formulaire
+      // Reset des champs du formulaire
+      setState({ nom: "", email: "", message: "" }); 
+    }
   };
 
   return (
     <div>
       <h1 className="text-contact">Contactez-moi !!</h1>
-    <form onSubmit={handleSubmit}>
-      <label>
-        Nom:
-        <input
-          type="text"
-          name="nom"
-          value={state.nom}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Email:
-        <input
-          type="email"
-          name="email"
-          value={state.email}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Message:
-        <textarea
-          name="message"
-          value={state.message}
-          onChange={handleInputChange}
-        />
-      </label>
-      <button type="submit">Envoyer</button>
-      {message && <p>{message}</p>}
-    </form>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nom:
+          <input
+            type="text"
+            name="nom"
+            value={state.nom}
+            onChange={handleInputChange}
+          />
+          {errorNom && <p style={{ color: 'red' }}>{errorNom}</p>}
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={state.email}
+            onChange={handleInputChange}
+          />
+          {errorEmail && <p style={{ color: 'red' }}>{errorEmail}</p>}
+        </label>
+        <label>
+          Message:
+          <textarea
+            name="message"
+            value={state.message}
+            onChange={handleInputChange}
+          />
+          {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+        </label>
+        <button type="submit">Envoyer</button>
+        {successMessage && <p style={{ color: 'darkgreen', marginTop: '5px', fontSize:'30px'}}>{successMessage}</p>}
+      </form>
     </div>
   );
 }
